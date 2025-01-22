3065b2336064eee93ef2a6ae023e385e9e1260c3
Lysande2902 <yenglee2006@gmail.com>
2025-01-22 12:52:51 -0600

    Comentario

---
+++ Triangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("¿Cuántos triángulos deseas calcular? ");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write("Lado a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Lado b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Lado c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine($"Área del triángulo {i + 1}: {area:F2}");
+        }
+    }
+}
